e3d904bc6d8d30f5490ecaf29d8dee94e5cc0460
Norbert <17ruud@gmail.com>
2023-01-29 21:06:04 +0100

    29.01

--- SupermarketAnalysis.sql
+++ SupermarketAnalysis.sql
@@ -1,11 +1,8 @@
 ---Which year had the highest sales?
 
---How was the weather during the year of highest sales?
-
---Conclude whether the weather has an essential impact on sales.
-
 --Do the sales always rise near the holiday season for all the years?
 
 --Analyze the relationship between sales and the different macroeconomic variables in the dataset.
 
-
+SELECT * FROM supermarket
+LIMIT 10
