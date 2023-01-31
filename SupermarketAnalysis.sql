ea25cf31a4acc26db03f7da7b1c35ed20db61bd9
Norbert <17ruud@gmail.com>
2023-01-31 20:33:14 +0100

    31.01

--- SupermarketAnalysis.sql
+++ SupermarketAnalysis.sql
@@ -31,6 +31,52 @@ ORDER BY total_sum DESC;
 
 ---
 
+--- Total sales based on gender and products categories.
 
+SELECT gender,product_line,SUM(total) AS total_sales 
+FROM supermarket 
+GROUP BY gender,product_line
+ORDER BY total_sales DESC;
 
+---
 
+--- What is the most popular way of payment based on gender and customer type.
+
+SELECT gender,customer,
+COUNT (CASE WHEN payment = 'Ewallet' THEN 1 END) AS Ewallet,
+COUNT (CASE WHEN payment = 'Cash' THEN 1 END) AS Cash,
+COUNT (CASE WHEN payment = 'Credit card' THEN 1 END) AS Credit_card
+FROM supermarket
+GROUP BY gender,customer;
+
+---
+--- Average spending by gender on each category of product
+
+SELECT gender,product_line,ROUND(AVG(total),2) AS average_spending 
+FROM supermarket
+GROUP BY gender,product_line
+ORDER BY gender,average_spending DESC;
+
+---
+--- How many products were bought from each category each day
+
+SELECT date_,
+ROUND(AVG(CASE WHEN product_line = 'Health and beauty' THEN total_products END),0)
+AS Health_and_beauty,
+ROUND(AVG(CASE WHEN product_line = 'Electronic accessories' THEN total_products END),0) 
+AS Electronic_accessories,
+ROUND(AVG(CASE WHEN product_line = 'Fashion accessories' THEN total_products END),0) 
+AS Fashion_accessories,
+ROUND(AVG(CASE WHEN product_line = 'Food and beverages' THEN total_products END),0) 
+AS Food_and_bevarages,
+ROUND(AVG(CASE WHEN product_line = 'Sports and travel' THEN total_products END),0) 
+AS Sports_and_travel,
+ROUND(AVG(CASE WHEN product_line = 'Home and lifestyle' THEN total_products END),0) 
+AS Home_and_lifestyle
+FROM
+(SELECT date_,SUM(quantity) AS total_products,product_line FROM supermarket
+GROUP BY date_,product_line
+ORDER BY date_) AS sub
+GROUP BY date_;
+
+---
